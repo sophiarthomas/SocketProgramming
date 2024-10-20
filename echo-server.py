13b5e6dc40dbdb38fbebdc3022e1a461188905d9
Sophia Thomas <sophiart03@gmail.com>
2024-10-20 14:11:33 -0700

    Update server

--- echo-server.py
+++ echo-server.py
@@ -7,15 +7,17 @@ import ipaddress
 
 # Server sets up a listening socket
 
-Host = ''
+Host = '0.0.0.0'
 
-Port = int(input("Enter port: "))
+Port = 8080
 
 ## creates listening socket (IPv4, tcp socket stream)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcpSock: 
     tcpSock.bind((Host, Port)) # binds the created socket to the host and port you want to listen to 
-    print(f"binding: {Host}, {Port}")
-    tcpSock.listen() # listen to a request for the server from this socket
+    print(f"binding: {Host}:{Port}")
+    tcpSock.listen(1) # listen to a request for the server from this socket
+    print("Server is listening for connections...")
+
     incomingSocket, clientAddr = tcpSock.accept() 
     with incomingSocket: 
         print(f"Connected by {clientAddr}")
@@ -24,6 +26,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcpSock:
             if not data: 
                 print("Connection Lost")
                 break
+            print(f"Recieved: {data.decode('utf-8')}")
+            print(f"Sent: {data.upper().decode('utf-8')}")
             incomingSocket.sendall(data.upper())
 	 
 	
